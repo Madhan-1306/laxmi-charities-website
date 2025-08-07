8d5c3fe8a64fa2336e95ad8607d5c91f25aee1f1
Madhan-1306 <madhanraj13062005@gmail.com>
2025-08-07 19:55:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+  document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll('.impact-count');
+
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-count');
+        const count = +counter.innerText.replace("+", "");
+        const increment = target / 200;
+
+        if (count < target) {
+          counter.innerText = Math.ceil(count + increment);
+          setTimeout(updateCount, 10);
+        } else {
+          counter.innerText = target.toLocaleString() + "+";
+        }
+      };
+
+      updateCount();
+    });
+  });
